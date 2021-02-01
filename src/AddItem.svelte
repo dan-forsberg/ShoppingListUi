3218d81fe9f65877d205b20af164aae983c703c7
Dan Örjestad <danorjest@gmail.com>
2021-02-01 13:25:40 +0100

    Passing ListItem from AddItem to CreateList successfully

--- src/AddItem.svelte
+++ src/AddItem.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
-    import type ListItem from "./models/ListItem";
-    export let item: ListItem;
-</script>
+import type ListItem from "./models/ListItem";
+    export let listItem: ListItem = {
+        item: null,
+        amount: null,
+        cost: null
+    };
+    </script>
 
 <label>
-    <input class="item_name" type="text" name="item" bind:value={item.item} />
-    <input class="item_amount" type="number" name="amount" bind:value={item.amount} />
-    <input class="item_cost" type="number" name="cost" bind:value={item.cost} />
+    <input class="item_name" type="text" name="item" bind:value={listItem.item} />
+    <input class="item_amount" type="number" name="amount" bind:value={listItem.amount} />
+    <input class="item_cost" type="number" name="cost" bind:value={listItem.cost} />
 </label>
+
+<style>
+    
+</style>
--- src/CreateList.svelte
+++ src/CreateList.svelte
@@ -1,28 +1,32 @@
 <script lang="ts">
-import AddItem from "./AddItem.svelte";
-import type ListItem from "./models/ListItem";
-import type ShoppingList from "./models/ShoppingList";
-let name:string;
-let items:Array<ListItem> = [];
-let itemsLength:number = items.length + 1;
+    import AddItem from "./AddItem.svelte";
+    import type ListItem from "./models/ListItem";
+    import type ShoppingList from "./models/ShoppingList";
+    let name: string;
+    let items: Array<ListItem> = [];
+    let itemsLength: number = items.length + 1;
+    let listItem:ListItem;
 
-function handleOnSubmit() {
-    let shoppingList:ShoppingList= {
-        name: name,
-        items: items
-    };
-}
+    function constructItems() {}
 
-function addItemComponent() {
-    ++itemsLength;
-}
+    function handleOnSubmit() {
+        constructItems();
+
+        let shoppingList: ShoppingList = {
+            name: name,
+            items: items,
+        };
+    }
+
+    function addItemComponent() {
+        console.log(listItem);
+    }
 </script>
+
 <form on:submit|preventDefault={handleOnSubmit}>
     <label>
-        <input type="text" bind:value={name}/>
-        {#each Array(itemsLength) as index }
-            <AddItem bind:value={items[index]}/>
-            <button on:click={addItemComponent} />
-        {/each}
+        <input type="text" bind:value={name} />
+        <AddItem bind:listItem={listItem}/>
+        <button on:click={addItemComponent} />
     </label>
 </form>
