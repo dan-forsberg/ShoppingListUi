9c6ccd0f5c4763c1f985cef8fd9958455f0e93e2
Dan Örjestad <danorjest@gmail.com>
2021-02-01 15:29:16 +0100

    added placeholders

--- src/AddItem.svelte
+++ src/AddItem.svelte
@@ -8,11 +8,11 @@ import type ListItem from "./models/ListItem";
     </script>
 
 <label>
-    <input class="item_name" type="text" name="item" bind:value={listItem.item} />
-    <input class="item_amount" type="number" name="amount" bind:value={listItem.amount} />
-    <input class="item_cost" type="number" name="cost" bind:value={listItem.cost} />
+    <input placeholder="Att köpa" class="item_name" type="text" name="item" bind:value={listItem.item} />
+    <input placeholder="Mängd" class="item_amount" type="number" name="amount" bind:value={listItem.amount} />
+    <input placeholder="Pris" class="item_cost" type="number" name="cost" bind:value={listItem.cost} />
 </label>
 
 <style>
-    
+
 </style>
