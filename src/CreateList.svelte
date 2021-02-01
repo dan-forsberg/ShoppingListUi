a6d01ec8460863b3a994b82c151c457153ac51fd
Dan Örjestad <danorjest@gmail.com>
2021-02-01 15:03:42 +0100

    Works, but not as I'd like

--- src/CreateList.svelte
+++ src/CreateList.svelte
@@ -1,32 +1,40 @@
 <script lang="ts">
+    import { writable } from "svelte/store";
     import AddItem from "./AddItem.svelte";
     import type ListItem from "./models/ListItem";
     import type ShoppingList from "./models/ShoppingList";
-    let name: string;
-    let items: Array<ListItem> = [];
-    let itemsLength: number = items.length + 1;
-    let listItem:ListItem;
+    /*TODO: support multiple AddItem components
+      Use items[index].items instead of local listItem? 
+      Probably needs a Svelte Store?*/
 
-    function constructItems() {}
+    let listItemStore = writable({});
+    $listItemStore = [{ item: null, amount: null, cost: null }];
+    let name: string;
+    let listItems: Array<ListItem> = [{ item: null, amount: null, cost: null }];
 
     function handleOnSubmit() {
-        constructItems();
-
         let shoppingList: ShoppingList = {
             name: name,
-            items: items,
+            items: listItems,
         };
+
+        console.log(shoppingList);
     }
 
     function addItemComponent() {
-        console.log(listItem);
+        //@ts-ignore -- this actually works
+        let len = $listItemStore.length;
+        $listItemStore[len] = { item: null, amount: null, cost: null };
     }
 </script>
 
 <form on:submit|preventDefault={handleOnSubmit}>
     <label>
         <input type="text" bind:value={name} />
-        <AddItem bind:listItem={listItem}/>
+
+        {#each $listItemStore as _, index}
+            <AddItem bind:listItem={listItems[index]} />
+        {/each}
         <button on:click={addItemComponent} />
     </label>
 </form>
