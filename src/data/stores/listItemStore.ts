2ba63acd1125c02a8250af7d0168e3b44e472735
Dan Örjestad <danorjest@gmail.com>
2021-02-02 07:42:37 +0100

    removed unneccessary inits

--- src/data/stores/listItemStore.ts
+++ src/data/stores/listItemStore.ts
@@ -2,5 +2,5 @@ import { writable } from "svelte/store";
 import type ListItem from "../../models/ListItem";
 
 export const listItemStore = writable<ListItem[]>([
-    { item: null, amount: null, cost: null },
+    { item: null },
 ]);
