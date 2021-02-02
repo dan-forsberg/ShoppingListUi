2b6338bf9e3f515db94122554c49fa9417fa8bdc
Dan Örjestad <danorjest@gmail.com>
2021-02-02 14:51:16 +0100

    Added toggle to createList
    WIP, everything needs styling, CreateList should be a modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import Api from "./data/api";
     import ShoppingLists from "./ShoppingLists.svelte";
-    import CreateList from "./CreateList.svelte";    
+    import CreateList from "./CreateList.svelte";
     import { ListStore, ListStoreState } from "./data/stores/listStore";
 
     let shoppingLists = Api.getLists();
@@ -14,10 +14,19 @@
             console.error(err);
             ListStoreState.set("ERROR");
         });
+
+    let createList = false;
+    function toggleCreateList() {
+        createList = !createList;
+    }
 </script>
 
-<CreateList />
-<ShoppingLists />
+<button on:click={toggleCreateList}>+</button>
+{#if createList}
+    <CreateList /> 
+{:else}
+    <ShoppingLists />
+{/if}
 
 <style>
 </style>
