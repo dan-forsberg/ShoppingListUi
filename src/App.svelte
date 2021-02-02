5ff4d5ae6fcd025976b483eb83ca1a13e3da9212
Dan Örjestad <danorjest@gmail.com>
2021-02-02 15:08:25 +0100

    awful modal, terrible, looks crap

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
     import ShoppingLists from "./ShoppingLists.svelte";
     import CreateList from "./CreateList.svelte";
     import { ListStore, ListStoreState } from "./data/stores/listStore";
+    import { slide } from 'svelte/transition';
 
     let shoppingLists = Api.getLists();
     shoppingLists
@@ -23,10 +24,44 @@
 
 <button on:click={toggleCreateList}>+</button>
 {#if createList}
-    <CreateList /> 
-{:else}
-    <ShoppingLists />
+    <div transition:slide class="modal">
+        <div class="modal-content">
+            <CreateList />
+        </div>
+    </div>
 {/if}
+<ShoppingLists />
 
 <style>
+    .modal {
+        display: block;
+        background-color: #010101;
+        margin: 15% auto; /* 15% from the top and centered */
+        padding: 20px;
+        border: 1px solid #888;
+        width: 80%; /* Could be more or less, depending on screen size */
+    }
+
+    .modal-content {
+        background-color: #fefefe;
+        margin: 15% auto; /* 15% from the top and centered */
+        padding: 20px;
+        border: 1px solid #888;
+        width: 80%; /* Could be more or less, depending on screen size */
+    }
+
+    /* The Close Button */
+    .close {
+        color: #aaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+    }
+
+    .close:hover,
+    .close:focus {
+        color: black;
+        text-decoration: none;
+        cursor: pointer;
+    }
 </style>
