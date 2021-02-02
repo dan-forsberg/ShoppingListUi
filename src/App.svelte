f6e981c94eaf01ecbd6978ab0fa27ac027f14de7
Dan Örjestad <danorjest@gmail.com>
2021-02-02 10:47:32 +0100

    Refactored out the "all shopping lists" into a new component

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,10 @@
 <script lang="ts">
-    import Api from "./data/api";
+    import ShoppingLists from "./ShoppingLists.svelte";
     import CreateList from "./CreateList.svelte";
-    import ShoppingListComponent from "./ShoppingList.svelte";
-    import { listStore } from "./data/stores/listStore";
-
-    let shoppingLists = Api.getLists();
-    let finishedFetching: boolean | undefined = undefined;
-    shoppingLists
-        .then((response) => {
-            listStore.set(response.lists);
-            finishedFetching = true;
-        })
-        .catch((err) => {
-            console.error(err);
-            finishedFetching = false;
-        });
 </script>
 
 <CreateList />
-{#if finishedFetching === undefined}
-    <p>Hämtar handlingslistor...</p>
-{:else if finishedFetching === true}
-    {#each $listStore as list}
-        <ShoppingListComponent {list} />
-    {/each}
-{:else}
-    <p>Hoppsan, något har visst gått jättefel.</p>
-{/if}
+<ShoppingLists />
 
 <style>
 </style>
---
+++ src/ShoppingLists.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Api from "./data/api";
+    import ShoppingListComponent from "./ShoppingList.svelte";
+    import { listStore } from "./data/stores/listStore";
+
+    let shoppingLists = Api.getLists();
+    let finishedFetching: boolean | undefined = undefined;
+    shoppingLists
+        .then((response) => {
+            listStore.set(response.lists);
+            finishedFetching = true;
+        })
+        .catch((err) => {
+            console.error(err);
+            finishedFetching = false;
+        });
+</script>
+
+{#if finishedFetching === undefined}
+    <p>Hämtar handlingslistor...</p>
+{:else if finishedFetching === true}
+    {#each $listStore as list}
+        <ShoppingListComponent {list} />
+    {/each}
+{:else}
+    <p>Hoppsan, något har visst gått jättefel.</p>
+{/if}
+
+<style>
+</style>
+
