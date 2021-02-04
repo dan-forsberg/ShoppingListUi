1ca5a4925a26bc65aeadf7c50394d2c1619ee3a2
Dan Örjestad <danorjest@gmail.com>
2021-02-04 11:33:09 +0100

    cost -> price

--- src/AddItem.svelte
+++ src/AddItem.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-    import type ListItem from "./models/ListItem";
+    import type ListItem from './models/ListItem';
     export let listItem: ListItem = {
         item: null,
         amount: null,
-        cost: null,
+        price: null,
+        bought: false,
     };
 </script>
 
@@ -16,29 +17,25 @@
         name="item"
         bind:value={listItem.item}
     />
-    <!-- svelte-ignore a11y-positive-tabindex -->
-    <!-- let the item-name take the focus always -->
     <input
-        tabindex="1"
         placeholder="Mängd"
         class="item-amount"
         type="number"
         name="amount"
         bind:value={listItem.amount}
     />
-    <!-- svelte-ignore a11y-positive-tabindex -->
     <input
-        tabindex="1"
         placeholder="Pris"
         class="item-cost"
         type="number"
-        name="cost"
-        bind:value={listItem.cost}
+        name="price"
+        bind:value={listItem.price}
     />
 </label>
 
 <style>
-    .item-amount, .item-cost {
+    .item-amount,
+    .item-cost {
         width: 70px;
         appearance: textfield;
     }
